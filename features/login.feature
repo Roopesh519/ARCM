5e8d553328437b473b29e89a8808ea8f7060bcb4
Roopesh <rsroopesh565@gmail.com>
2024-06-30 23:54:52 +0530

    update

--- features/login.feature
+++ features/login.feature
@@ -7,14 +7,14 @@ Feature: Login to ARCM
         When I enter my email as "roopesh.yadava@7edge.com"
         And I enter my password as "Admin@123"
         And I click on the "login" button
-        Then I should see a pop up window
+        Then I should see a a dialog box to select preference for otp verification 
         When I click on "Select Preference" button
         And I select my Preference
         And I click on "send" button
         And I navigate to otp verification page
         And I enter otp as "981256"
-        And I click on verify otp
-        Then I see a message "Do you want to trust this browser"
+        And I click on "verify otp" button
+        Then I see a message Do you want to trust this browser
         When I click on "No, I dont" button
         Then I navigate to profile page
         And I should see a message "Login Successfull"
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -9,8 +9,8 @@ Given('I am on the login page', async function () {
   await this.driver.get('https://cs0275-dev-organization.accessibleremotecaremanagement.net/');
   await new Promise(resolve => setTimeout(resolve, 500));
   await this.driver.wait(until.elementLocated(By.xpath('//*[text()="Login"]')));
-
 });
+
 
 // Enter email
 When('I enter my email as {string}', async function (email) {
@@ -19,6 +19,7 @@ When('I enter my email as {string}', async function (email) {
   await emailInput.sendKeys(email);
 });
 
+
 // Enter password
 When('I enter my password as {string}', async function (password) {
   const passwordInput = await driver.wait(until.elementLocated(By.id('passWord')));
@@ -26,25 +27,65 @@ When('I enter my password as {string}', async function (password) {
   await passwordInput.sendKeys(password);
 });
 
+
 // Click on the specified button
+
 When('I click on the {string} button', async function (button) {
-  const buttonElement = await driver.wait(until.elementLocated(By.id(button)));
+  let buttonElement;
+  switch(button) {
+      case 'login':
+          buttonElement = await this.driver.wait(until.elementLocated(By.id('login')));
+          break;
+      case 'Select Preference':
+          buttonElement = await this.driver.wait(until.elementLocated(By.css('[data-testid="select-device-btn"]')));
+          break;
+      case 'send':
+          buttonElement = await this.driver.wait(until.elementLocated(By.id('add_admin')));
+          break;
+      case 'verify otp':
+          buttonElement = await this.driver.wait(until.elementLocated(By.id('otp-form')));
+          break;
+      case 'No, I dont':
+          buttonElement = await this.driver.wait(until.elementLocated(By.id('cancel')));
+          break;
+      
+      default:
+          console.log('Invalid button string');
+          return; 
+  }
   await buttonElement.click();
 });
 
 
-Then('I should see a pop up window', async function () {
-  const window = await driver.wait(until.elementLocated(By.xpath(`//*[contains(text(), "Verify your Identity")]`)));
+Then('I should see a a dialog box to select preference for otp verification ', async function () {
+  const dialog = await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Verify your Identity")]')));
 });
 
-When('I click on {string} button', async function (selectPreference) {
-  const preference = await drivit.wait(until.elementLocated(By.id))
+
+When('I select my Preference', async function () {
+  const selectElement = await this.driver.wait(until.elementLocated(By.css('[data-testid="device_template"]'))).click();
 });
 
-And I select my Preference
-And I click on "send" button
-And I navigate to otp verification page
-And I enter otp as "981256"
+
+When('I navigate to otp verification page', async function () {
+  const window = await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "OTP Verification")]')));
+});
+
+
+When('I enter otp as {string}', async function (otp) {
+  const otpDigits = otp.split('');
+  for (let i = 0; i < otpDigits.length; i++) {
+    const otpInput = await this.driver.wait(until.elementLocated(By.id(i.toString())));
+    await otpInput.sendKeys(Key.chord(Key.CONTROL, 'a'), Key.DELETE);
+    await otpInput.sendKeys(otpDigits[i]);
+  }
+});
+
+
+Then('I see a message Do you want to trust this browser', async function () {
+  const trustMessage = await this.driver.wait(until.elementLocated(By.xpath('//*[contains(text(), "Do you want to trust this browser")]')));
+  assert(trustMessage !== null);
+});
 
 
 // Verify navigation to profile page
@@ -54,9 +95,13 @@ Then('I navigate to profile page', async function () {
   expect(currentUrl).to.include('profile');
 });
 
+
 // Verify message text
 Then('I should see a message {string}', async function (message) {
-  const successMessage = await this.driver.wait(until.elementLocated(By.xpath(`//*[contains(text(), "${message}")]`)));
-  const text = await successMessage.getText();
-  expect(text).to.equal(message);
+  for (let loop = 100; loop > 0; loop--) {
+      await driver.manage().setTimeouts({ pageLoad: 300 });
+      let pageSource = await driver.getPageSource();
+      let check = pageSource.includes(message); 
+      assert.ok(check, 'passed');
+    }
 });
