41915146a94ba00fed3f0a897919982dcc109a6f
Roopesh <rsroopesh565@gmail.com>
2024-07-01 18:42:05 +0530

    updated

--- features/step_definitions/manageObserver.js
+++ features/step_definitions/manageObserver.js
@@ -6,11 +6,13 @@ const { expect } = require('chai');
 
 setDefaultTimeout(60 * 1000);
 
+
 Given('I am on the profile page', async function () {
     await this.driver.get('https://cs0275-dev-organization.accessibleremotecaremanagement.net/profile');
     await new Promise(resolve => setTimeout(resolve, 500));
     await this.driver.wait(until.elementLocated(By.xpath('//*[text()="My Profile"]')));
 });
+
 
 When('I click on {string} button', async function(button) {
     let buttonElement;
@@ -25,11 +27,13 @@ When('I click on {string} button', async function(button) {
     await buttonElement.click();
 });
 
+
 When('I navigate to manage observer page', async function () {
     await this.driver.wait(until.urlContains('manage-observer'));
     await this.driver.wait(until.elementLocated(By.xpath('//*[text()="Manage Observers"]')));
     await this.driver.wait(until.elementLocated(By.css('table.table-auto')), 5000); // Adjust as necessary
 });
+
 
 Then('I should see a table with the following columns:', async function (dataTable) {
     // Verify the table headers
@@ -40,11 +44,13 @@ Then('I should see a table with the following columns:', async function (dataTab
     }
 });
 
+
 Then('the table should display a list of observers', async function () {
     // Verify the table rows
     const rows = await this.driver.wait(until.elementsLocated(By.css('table.table-auto tbody tr')));
     expect(rows.length).to.be.greaterThan(0);
 });
+
 
 Then('I should see a total number of records displayed at the bottom', async function () {
     // Verify the total number of records
