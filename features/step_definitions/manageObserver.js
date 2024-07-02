b5cf77efbd5ddce76b288715f7a83086f6aad377
Roopesh <rsroopesh565@gmail.com>
2024-07-02 12:02:51 +0530

    update

--- features/step_definitions/manageObserver.js
+++ features/step_definitions/manageObserver.js
@@ -2,7 +2,6 @@ const assert = require('assert');
 const { Before, Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
 const { By, Key, Builder, until, Select } = require('selenium-webdriver');
 const { faker } = require('@faker-js/faker');
-const { expect } = require('chai');
 
 setDefaultTimeout(60 * 1000);
 
