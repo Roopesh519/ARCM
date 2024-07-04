e1ef92ac048b71c0ce10ce7162e25bcb9aacaf15
Roopesh <rsroopesh565@gmail.com>
2024-07-04 09:32:29 +0530

    updated

--- features/step_definitions/manageObserver.js
+++ features/step_definitions/manageObserver.js
@@ -18,6 +18,30 @@ When('I click on manage observer', async function () {
 });
 
 
+Given('I am on the manage observer page', async function () {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    await global.driver.get('https://cs0275-dev-organization.accessibleremotecaremanagement.net/manage-observer')
+    await global.driver.wait(until.elementLocated(By.xpath(`//h1[text()="Manage Observers"]`)))
+});
+
+
+When('I click on {string} button', async function (button) {
+    let buttonElement;
+    switch (button) {
+        case 'Search':
+            buttonElement = await driver.wait(until.elementLocated(By.id('search-value')));
+            break;
+        case 'Filter':
+            buttonElement = await global.driver.wait(until.elementLocated(By.id('filter')));
+            break;
+        default:
+            console.log('Invalid button string');
+            return;
+    }
+    await buttonElement.click();
+});
+
+
 Then('I should see a table with the following columns:', async function (dataTable) {
     // Verify the table headers
     const headers = dataTable.raw().flat();
@@ -45,6 +69,52 @@ Then('I should see a total number of records displayed at the bottom', async fun
 
 
 
-When('I enter text as {string}', function (string) {
-    return 'pending';
+When('I enter text as {string}', async function (text) {
+    const searchInput = await global.driver.wait(until.elementLocated(By.id('search-value')));
+    await searchInput.sendKeys(Key.chord(Key.CONTROL, 'a'), Key.DELETE);
+    await searchInput.sendKeys(text);
 });
+
+//------
+
+       
+         Then('I should see a pop up window for Filter', async function () {
+            await global.driver.wait(until.elementLocated(By.xpath("//*[contains(text(), 'Filter by:')]")));
+         });
+       
+       
+         When('I enter start date as {string}', async function (date) {
+            const dateFilter = await global.driver.wait(until.elementLocated(By.id('filter_start_date')));
+            await dateFilter.sendKeys(Key.chord(Key.CONTROL, 'a'), Key.DELETE);
+            await dateFilter.sendKeys(date);
+         });
+
+
+         When('I enter end date as {string}', async function (date) {
+            const dateFilter = await global.driver.wait(until.elementLocated(By.id('filter_end_date')));
+            await dateFilter.sendKeys(Key.chord(Key.CONTROL, 'a'), Key.DELETE);
+            await dateFilter.sendKeys(date);
+         });
+
+       
+         Then('I should see the filter applied', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+
+         When('I select status as {string}', async function (status) {
+            let buttonElement;
+            switch (status) {
+                case 'Active':
+                    buttonElement = await global.driver.wait(until.elementLocated(By.xpath("//label[text()='Active']")));
+                    break;
+                case 'Inactive':
+                    buttonElement = await global.driver.wait(until.elementLocated(By.xpath("//label[text()='Inactive']")));
+                    break;
+                default:
+                    console.log('Invalid button string');
+                    return;
+            }
+            await buttonElement.click();
+        });
